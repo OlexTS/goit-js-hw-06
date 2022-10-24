da11f8ded77858098ceca619beee64f56f847bb4
OlexTS <sashokseven@gmail.com>
2022-10-24 19:09:16 +0300

    made task-6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const validatedInputRef = document.querySelector('#validation-input');
+
+const onInputFocusOut = () => {
+   if (validatedInputRef.value.length >= validatedInputRef.dataset.length) {
+        validatedInputRef.classList.add('valid');
+        validatedInputRef.classList.remove('invalid');
+    } else {
+        validatedInputRef.classList.add('invalid');
+        validatedInputRef.classList.remove('valid');
+    }
+    
+}
+validatedInputRef.addEventListener('blur', onInputFocusOut)
