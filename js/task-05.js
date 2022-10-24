4a48d96f1b11b537305ceec60b1a1ddfdaac6ff5
OlexTS <sashokseven@gmail.com>
2022-10-25 01:10:40 +0300

    made 10 task

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,8 @@ const inputRef = document.querySelector("#name-input");
 const spanRef = document.querySelector("#name-output");
 
 const onInputChange = (event) => {
-  console.log(event);
-
-  event.currentTarget.value === ""
+  
+  !event.currentTarget.value
     ? (spanRef.textContent = "Anonymous")
     : (spanRef.textContent = event.currentTarget.value);
 };
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxAmountInputRef = document.querySelector('input[type="number"]');
+const buttonCreateBoxRef = document.querySelector("[data-create]");
+const buttonDestroyBoxRef = document.querySelector("[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  const boxesArr = [];
+  amount = boxAmountInputRef.value;
+
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 0; i < amount; i += 1) {
+      const box = document.createElement("div");
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${30 + 10 * i}px`;
+      box.style.height = `${30 + 10 * i}px`;
+      boxesArr.push(box);
+    }
+
+    boxesRef.append(...boxesArr);
+  } else {
+    alert("limit exceeded");
+  };
+};
+
+const destroyBoxes = () => {
+  boxesRef.innerHTML = "";
+};
+
+buttonCreateBoxRef.addEventListener("click", createBoxes);
+buttonDestroyBoxRef.addEventListener("click", destroyBoxes);
